6078d9d8755732b9139329f15ea32f08bbf9f653
shashankthakare <shashankthakare1@gmail.com>
2017-12-04 22:18:11 -0800

    error handling and Create log and permissions onCreate if not exists

--- app/src/main/java/de/robv/android/xposed/installer/PermissionListActivity.java
+++ app/src/main/java/de/robv/android/xposed/installer/PermissionListActivity.java
@@ -3,6 +3,8 @@ package de.robv.android.xposed.installer;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.JsonWriter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +13,15 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.io.File;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Arijeet on 10/31/2017.
@@ -21,20 +29,47 @@ import java.util.Map;
 public class PermissionListActivity extends ListActivity {
 
     public void onCreate(Bundle icicle) {
+        try {
+
+            File logFile = new File(XposedApp.BASE_DIR + "log/permlog.json");
+            if (!logFile.exists()) {
+                logFile.createNewFile();
+                logFile.setReadable(true, false);
+                logFile.setWritable(true, false);
+                logFile.setExecutable(true, false);
+                JsonWriter jwriter = new JsonWriter(new OutputStreamWriter(new FileOutputStream(logFile)));
+                jwriter.setIndent("  ");
+                jwriter.beginArray();
+                jwriter.beginObject();
+                jwriter.endObject();
+                jwriter.endArray();
+                jwriter.close();
+            }
+            File permissionFile = new File(XposedApp.BASE_DIR + "conf/permissions.json");
+            if (!permissionFile.exists()) {
+                PermissionManagerUtil.savePermissions();
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error occurred while creating Log and permssion json files");
+        }
+
         setTitle("Permission Manager");
         super.onCreate(icicle);
         // create an array of Strings, that will be put to our ListActivity
-        ArrayAdapter<Model> adapter = new InteractiveArrayAdapter(this,
-                getModel());
-        setListAdapter(adapter);
+        try {
+            ArrayAdapter<Model> adapter = new InteractiveArrayAdapter(this,getModel());
+            setListAdapter(adapter);
+        } catch (Exception e) {
+            Log.e(TAG, "onCreate ke yaha phata");
+        }
     }
 
     private List<Model> getModel() {
-        Map<String, Map<String , Boolean>> logmap= PermissionManagerUtil.getLogMap();
+        Map<String, Map<String, Boolean>> logmap = PermissionManagerUtil.getLogMap();
         List<Model> list = new ArrayList<Model>();
-        for(String module: logmap.keySet()){
-            for(String pname: logmap.get(module).keySet()){
-                boolean allowed= logmap.get(module).get(pname);
+        for (String module : logmap.keySet()) {
+            for (String pname : logmap.get(module).keySet()) {
+                boolean allowed = logmap.get(module).get(pname);
                 list.add(new Model(module, pname, allowed));
             }
         }
@@ -46,10 +81,10 @@ public class PermissionListActivity extends ListActivity {
         public String packname;
         public boolean isAllowed;
 
-        public Model(String mname, String pname, Boolean allow){
-            module= mname;
-            packname= pname;
-            isAllowed= allow;
+        public Model(String mname, String pname, Boolean allow) {
+            module = mname;
+            packname = pname;
+            isAllowed = allow;
         }
     }
 
@@ -88,7 +123,7 @@ public class PermissionListActivity extends ListActivity {
                                 Model element = (Model) viewHolder.checkbox
                                         .getTag();
                                 boolean changed = element.isAllowed ^ buttonView.isChecked();
-                                if(changed) {
+                                if (changed) {
                                     element.isAllowed = buttonView.isChecked();
                                     PermissionManagerUtil.updatePermissions(
                                             element.module, element.packname, buttonView.isChecked());
